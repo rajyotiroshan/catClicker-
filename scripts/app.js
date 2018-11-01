4b9b93690811e3b29199a1b78f1b31b87fbfaa0b
rajan <rajangreedge@gmail.com>
2018-11-01 11:24:34 +0530

    project setup

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+$(function(){
+	/* model object to store cat data*/
+	let model = {
+		
+	}
+	/* octopus a medium bw model and view for communication.*/
+	let octopus = {
+
+	}
+	/* view represent UI.*/
+	let view = {
+
+	}
+
+	octopus.init();
+});
